1b2dbc9e77b9f4a6f1d9215976ed676b10bfe823
Benq <bqi343@gmail.com>
2019-01-07 21:24:23 -0500

    no message

--- code.java
+++ code.java
@@ -5,26 +5,71 @@ public class MyRobot extends BCAbstractRobot {
     public Robot[] robots;
     int[][] robotMap;
 
-    boolean valid (int x, int y) {
+    public boolean valid (int x, int y) {
         if (!(0 <= x && x < robotMap.length && 0 <= y && y < robotMap[0].length)) return false;
         return map[x][y];
     }
 
-    public boolean isEmpty(int x, int y) {
+    public boolean isNotEmpty (int x, int y) {
         return valid(x,y) && robotMap[x][y] != 0;
     }
 
+    public boolean isEmpty(int x, int y) {
+        return valid(x,y) && robotMap[x][y] == 0;
+    }
+
+    public int moveDist(Robot r) {
+        if (r.unit == SPECS.CRUSADER) return 9;
+        return 4;
+    }
+
     public boolean canMove(Robot r, int dx, int dy) {
+        if (moveDist(r) < dx*dx+dy*dy) return false;
         int x = r.x+dx, y = r.y+dy;
         return isEmpty(x,y);
     }
 
-    public void tryBuild(int type) {
+    public boolean canAttack(int dx, int dy) {
+        int x = me.x+dx, y = me.y+dy;
+        if (!isNotEmpty(x,y)) return false;
+
+        int dist = dx*dx+dy*dy;
+        if (me.unit == SPECS.CRUSADER) {
+            if (getRobot(robotMap[x][y]).team == me.team) return false;
+            if (dist < 1 || dist > 4) return false;
+        } else if (me.unit == SPECS.PROPHET) {
+            if (getRobot(robotMap[x][y]).team == me.team) return false;
+            if (dist < 4 || dist > 8) return false;
+            return true;
+        } else if (me.unit == SPECS.PREACHER) {
+            if (dist < 1 || dist > 4) return false;
+            return true;
+        }
+        return false;
+    }
+
+    public Action tryAttack() {
+        for (int dx = -2; dx <= 2; ++dx)
+            for (int dy = -2; dy <= 2; ++dy) 
+                if (canAttack(dx,dy)) 
+                    return attack(dx,dy);
+        return null;
+    }
+
+    public Action someMove() {
+        for (int dx = -3; dx <= 3; ++dx)
+            for (int dy = -3; dy <= 3; ++dy) 
+                if (canMove(me,dx,dy)) 
+                    return move(dx,dy);
+        return null;
+    }
+    
+    public Action tryBuild(int type) {
         for (int dx = -1; dx <= 1; ++dx)
             for (int dy = -1; dy <= 1; ++dy)
                     if (isEmpty(me.x+dx,me.y+dy))
-                        return buildUnit(type,);
-
+                        return buildUnit(type,dx,dy);
+        return null;
     }
 
     public Action runCastle() {
@@ -34,9 +79,12 @@ public class MyRobot extends BCAbstractRobot {
         }*/
 
         if (turn % 10 == 0) {
+            log("HI");
             Action A = tryBuild(SPECS.CRUSADER);
-            if (A != null) return A;
-
+            if (A != null) {
+                log("Built crusader");
+                return A;
+            }
         } 
 
         // this.log("Building a crusader at " + (this.me.x+1) + ", " + (this.me.y+1));
@@ -63,7 +111,9 @@ public class MyRobot extends BCAbstractRobot {
     }
 
     public Action runCrusader() {
-        return move(0,1);
+        Action A = tryAttack();
+        if (A != null) return A;
+        return someMove();
         /*const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
         const choice = choices[Math.floor(Math.random()*choices.length)];
         return this.move(...choice);*/
