42dfb6f1db84087027e6b9ef87aebb6695fc6cab
Benjamin Qi <bqi343@gmail.com>
2019-01-10 12:49:10 -0500

    don't step on resources

--- Attackable.java
+++ Attackable.java
@@ -58,10 +58,12 @@ public class Attackable extends Movable {
         int bestDist = MOD, bestPos = MOD;
         for (int X = x-5; X <= x+5; ++X) 
             for (int Y = y-5; Y <= y+5; ++Y) 
-                if (Z.passable(X,Y) && (X+Y-x-y) % 2 == 0 && (Z.sq(X-x)+Z.sq(Y-y) < bestDist) && Z.dist[Y][X] < 5) {
+                if (Z.passable(X,Y) && (X+Y-x-y) % 2 == 0 && Z.dist[Y][X] < 5) {
                     if (Z.sq(X-x)+Z.sq(Y-y) <= 2 && Z.numOpen(t) <= 2) continue;
-                    bestDist = Z.sq(X-x)+Z.sq(Y-y);
-                    bestPos = 64*X+Y;
+                    if (Z.sq(X-x)+Z.sq(Y-y) < bestDist || (Z.sq(X-x)+Z.sq(Y-y) == bestDist && !Z.karboniteMap[Y][X] && !Z.fuelMap[Y][X])) {
+                        bestDist = Z.sq(X-x)+Z.sq(Y-y);
+                        bestPos = 64*X+Y;
+                    }
                 }
 
         return nextMove(bestPos);
