e88e6156ead04226aac2600a2c2b9ae4c86e8d4c
Walden Yan <walnutwaldo20@gmail.com>
2019-01-17 16:28:16 -0500

    new update to bc19

--- Consts.java
+++ Consts.java
@@ -17,7 +17,7 @@ public class Consts {
     public static final int RED = 0;
     public static final int BLUE = 1;
 
-    public static final int[] CONSTRUCTION_K = {-1, 50, 10, 20, 25, 30};
+    public static final int[] CONSTRUCTION_K = {-1, 50, 10, 15, 25, 30};
     public static final int[] CONSTRUCTION_F = {-1, 200, 50, 50, 50, 50};
 
     public static final int K_CARRY_CAP = 20;
@@ -27,16 +27,16 @@ public class Consts {
     public static final int[] MOVE_SPEED = {-1, -1, 4, 9, 4, 4};
     public static final int[] MOVE_F_COST = {-1, -1, 1, 1, 2, 3};
 
-    public static final int[] START_HEALTH = {-1, -1, 10, 40, 20, 60};
+    public static final int[] START_HEALTH = {200, 100, 10, 40, 20, 60};
 
-    public static final int[] VISION_R = {100, 100, 100, 36, 64, 16};
+    public static final int[] VISION_R = {100, 100, 100, 49, 64, 16};
 
-    public static final int[] DAMAGE = {-1, -1, -1, 10, 10, 20};
+    public static final int[] DAMAGE = {10, -1, -1, 10, 10, 20};
 
     public static final boolean[] CAN_ATTACK = {false, false, false, true, true, true};
-    public static final int[] MIN_ATTACK_R = {-1, -1, -1, 1, 16, 1};
-    public static final int[] MAX_ATTACK_R = {-1, -1, -1, 16, 64, 16};
-    public static final int[] ATTACK_F_COST = {-1, -1, -1, 10, 25, 15};
+    public static final int[] MIN_ATTACK_R = {1, -1, -1, 1, 16, 1};
+    public static final int[] MAX_ATTACK_R = {64, -1, -1, 16, 64, 16};
+    public static final int[] ATTACK_F_COST = {10, -1, -1, 10, 25, 15};
 
     public static final int churchThreshold = 5; // min distance from nearest struct
 }
