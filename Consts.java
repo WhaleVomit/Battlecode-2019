15fb7cbffd784fb25cba348923dfdc8885b84395
Benq <bqi343@gmail.com>
2019-01-18 17:31:19 -0500

    produce more pilgrims

--- Consts.java
+++ Consts.java
@@ -39,5 +39,5 @@ public class Consts {
     public static final int[] MAX_ATTACK_R = {64, -1, -1, 16, 64, 16};
     public static final int[] ATTACK_F_COST = {10, -1, -1, 10, 25, 15};
 
-    public static final int churchThreshold = 1; // min distance from nearest struct
+    public static final int churchThreshold = 3; // min distance from nearest struct
 }
